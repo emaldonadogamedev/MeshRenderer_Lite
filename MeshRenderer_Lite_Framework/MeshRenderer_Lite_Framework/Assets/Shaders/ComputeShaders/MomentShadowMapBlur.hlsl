698d6a7814382d29acccc1e10c9800d8a5d8601c
emaldonadogamedev <esteban.maldonado@digipen.edu>
2018-10-14 22:19:25 -0700

    meh...will continue this shit tomorrow

--- MeshRenderer_Lite_Framework/MeshRenderer_Lite_Framework/Assets/Shaders/ComputeShaders/MomentShadowMapBlur.hlsl
+++ MeshRenderer_Lite_Framework/MeshRenderer_Lite_Framework/Assets/Shaders/ComputeShaders/MomentShadowMapBlur.hlsl
@@ -1,7 +1,9 @@
 
+Texture2D inputShadowMap;
+RWTexture2D<float4> outputShadowMap;
 
 [numthreads(128, 1, 1)]
-void main( uint3 DTid : SV_DispatchThreadID )
+void main( uint3 dispatchThreadId : SV_DispatchThreadID )
 {
 
 }
